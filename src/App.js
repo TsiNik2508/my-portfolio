a02ca76a74370ea2c9ed3db959e3045a6bf5bd22
Nikita <personalnick@yandex.ru>
2024-11-06 15:23:33 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
 import Header from "./components/Header/Header";
 import StarBackground from "./components/StarBackground/StarBackground";
